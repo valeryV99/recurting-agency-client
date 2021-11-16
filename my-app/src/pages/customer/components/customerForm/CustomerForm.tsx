a3272b60e64b48b0683ca9680cff19c30387624c
Valery Verenich <ver.valery99@gmail.com>
2021-11-16 22:26:10 +0300

    -

--- my-app/src/pages/customer/components/customerForm/CustomerForm.tsx
+++ my-app/src/pages/customer/components/customerForm/CustomerForm.tsx
@@ -1,30 +1,48 @@
 import { useForm } from 'react-hook-form'
+import { createCustomer } from '../../../../services/customersService'
 
 interface CustomerFormFields {
-  customerName: string;
-  customerAddress: string;
+  name: string;
+  address: string;
 }
 
 const CustomerForm = () => {
   const { watch, register, setValue, handleSubmit } =
     useForm<CustomerFormFields>({
       defaultValues: {
-        customerName: '',
-        customerAddress: '',
+        name: '',
+        address: '',
       },
     })
-  const { customerName, customerAddress } = watch()
+  const { name, address } = watch()
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data, 'data'))}>
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        console.log(data, 'data')
+        const customer = await createCustomer(data)
+        console.log(customer, 'customer')
+      })}
+    >
       <div>
         <label>Customer name</label>
-        <input type="text" value={customerName} />
+        <input
+          {...register('name')}
+          type="text"
+          value={name}
+          onChange={({ target: { value } }) => setValue('name', value)}
+        />
       </div>
       <div>
         <label>Customer address</label>
-        <input type="text" value={customerAddress} />
+        <input
+          {...register('address')}
+          type="text"
+          value={address}
+          onChange={({ target: { value } }) => setValue('address', value)}
+        />
       </div>
+      <button type="submit">Create</button>
     </form>
   )
 }
--- my-app/src/services/customersService.ts
+++ my-app/src/services/customersService.ts
@@ -1,43 +1,13 @@
+import requestService from './requestService'
+
 export interface Customer {
   id: string;
   name: string;
   address: string;
 }
 
-export const getCustomers = (): Promise<Customer[]> =>
-  new Promise((resolve) =>
-    setTimeout(
-      () =>
-        resolve([
-          {
-            id: '1',
-            name: 'Light Well Organization',
-            address: 'Belarus, Minsk',
-          },
-          {
-            id: '2',
-            name: 'Issoft',
-            address: 'the USA, Minnesota',
-          },
-          {
-            id: '3',
-            name: 'Sideways6',
-            address: 'the UK, London',
-          },
-          {
-            id: '4',
-            name: 'SmartIT',
-            address: 'the USA, san-francisco',
-          },
-          {
-            id: '5',
-            name: '3Commas',
-            address: 'Estonia, Tallinn',
-          },
-        ]),
-      1000
-    )
-  )
+export const getCustomers = () =>
+  requestService.get('customers').then(({ data }) => data)
 
 export const getCustomer = (
   customerId: string
@@ -76,3 +46,17 @@ export const getCustomer = (
     )
   )
 }
+
+export const createCustomer = ({
+  name,
+  address,
+}: {
+  name: string,
+  address: string,
+}) =>
+  requestService
+    .post('customers', {
+      customer_name: name,
+      address: address,
+    })
+    .then(({ data }) => data)
